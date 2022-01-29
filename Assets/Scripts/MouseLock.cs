8ab8512156c716ab612ea1225b3720b5d9101a50
ex4lyt3 <quantum.rise.21@gmail.com>
2022-01-29 21:34:23 +0800

    Patch v0.3.2

--- Assets/Scripts/MouseLock.cs
+++ Assets/Scripts/MouseLock.cs
@@ -16,7 +16,7 @@ public class MouseLock : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     // Update is called once per frame
@@ -37,5 +37,6 @@ public class MouseLock : MonoBehaviour
     public void ChangeSensitivity(float defaultMultiplier)
     {
         mouseSensitivity = defaultSensitivity * defaultMultiplier;
+        Debug.Log(mouseSensitivity);
     }
 }
--- Assets/Scripts/SettingsConfig.cs
+++ Assets/Scripts/SettingsConfig.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class SettingsConfig : MonoBehaviour
 {
     public GameObject Settings;
+    public InputField SensitivityInput;
+    public MouseLock MouseLock;
 
     bool SettingsState = false;
+    bool debounce = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +22,34 @@ public class SettingsConfig : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.E)){
-            SettingsState = !SettingsState;
+        if (Input.GetKey(KeyCode.E) && debounce == false){
+            debounce = true;
+            SettingsState = !(SettingsState);
+            Debug.Log(SettingsState);
             SettingsOpen(SettingsState);
+            Invoke("DebounceFalse",0.2f);
         }
     }
 
-    public void SettingsOpen(bool arg)
+    void SettingsOpen(bool arg)
     {
         Settings.SetActive(arg);
-        Cursor.visible = false;
+        Cursor.visible = arg;
+        if (arg == true){
+            Cursor.lockState = CursorLockMode.None;
+        } else {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    void DebounceFalse()
+    {
+        debounce = false;
+    }
+
+    public void OnSensitivityChange(string arg)
+    {
+        float sensitivity = float.Parse(SensitivityInput.text);
+        MouseLock.ChangeSensitivity(sensitivity);
     }
 }
